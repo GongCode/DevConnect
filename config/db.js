73ef19c91b95d9bdf038eee276de664726b2ac2a
Xingjian Gong <JBQX36@motorolasolutions.com>
2019-06-24 15:06:02 -0500

    Create new profile and updating profile

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     //mongoose.set('useCreateIndex', true);
 
     console.log('MongoDB Connected ..');
