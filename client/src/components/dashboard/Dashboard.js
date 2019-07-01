654126dc78df46dfacde087f2474ac6b8ff37d71
Xingjian Gong <JBQX36@motorolasolutions.com>
2019-06-30 20:35:40 -0500

    Display profiles and bug fixes

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,8 +16,7 @@ const Dashboard = ({
 }) => {
   useEffect(() => {
     getCurrentProfile();
-    // eslint-disable-next-line
-  }, []);
+  }, [getCurrentProfile]);
   return loading && profile == null ? (
     <Spinner />
   ) : (
